64552baca4fe772786d636f083b41cc683544516
Razvan FILIP <razvan.filip.70@gmail.com>
2022-07-22 14:25:59 +0300

    Reverse translate coordinates

--- closest-coffee-shops-ui/src/utils/CoordinateConverter.js
+++ closest-coffee-shops-ui/src/utils/CoordinateConverter.js
@@ -1,3 +1,9 @@
+/**
+ * Translate real coordinates to map coordinates
+ * @param {Number} x X coordinate
+ * @param {Number} y Y coordinate
+ * @returns the translated values
+ */
 export function translateMapCoordinates(x, y) {
     const translateX = (coord) => {
         return (parseInt(coord) + 180) * 10;
@@ -8,4 +14,22 @@ export function translateMapCoordinates(x, y) {
     };
 
     return [translateX(x), translateY(y)];
+}
+
+/**
+ * Translate map coordinates to real coordinates
+ * @param {Number} x X coordinate
+ * @param {Number} y Y coordinate
+ * @returns the translated values
+ */
+export function reverseTranslateMapCoordinates(x, y) {
+    const revTranslateX = (coord) => {
+        return (parseInt(coord) / 10 - 180).toFixed(2);
+    };
+
+    const revTranslateY = (coord) => {
+        return (parseInt(coord) / 10 - 180).toFixed(2);
+    };
+
+    return [revTranslateX(x), revTranslateY(y)];
 }
