3b248426c2c8795290bb019df4234be72eca2ae8
Razvan Filip <razvan.filip.70@gmail.com>
2022-08-24 17:19:26 +0300

    Implement useCoffeeShops hook

--- closest-coffee-shops-ui/src/App.js
+++ closest-coffee-shops-ui/src/App.js
@@ -1,58 +1,17 @@
 import "./App.css";
 
-import coffeeShops from "./services/closestCoffeeShops/coffeeShopsComponents/coffeeShops";
-import { NUMBER_OF_SHOPS_TO_HIGHLIGHT } from "./services/closestCoffeeShops/utils/config";
 import { CoffeeShop } from "./components/DrawCoffeeShopcomponent";
 import Pointer from "./components/DrawPointerComponent";
 import Map from "./components/Map";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { translateMouseCoordsAndCall } from "./utils/CoordinateConverter";
+import { useCoffeeShops } from "./components/useCoffeeShops";
 
 function App() {
-  const [shops, setShops] = useState([]);
   const [selectedPoint, setSelectedPoint] = useState(null);
   const [coords, setCoords] = useState({ x: 0, y: 0 });
-  const getClosestCoffeShops = useRef(null);
 
-  useEffect(() => {
-    const displayInitialCoffeeShops = async () => {
-      const coffeeshops = await coffeeShops();
-
-      getClosestCoffeShops.current = coffeeshops.getCoffeeShops;
-
-      setShops(getClosestCoffeShops.current({ x: 0, y: 0 }));
-    };
-
-    displayInitialCoffeeShops().catch(console.error);
-  }, []);
-
-  const useCoffeeShops = () => {
-    const determineHighlightedShops = (shops) => {
-      if (Array.isArray(shops)) {
-        const cshops = shops.map((item, index) => {
-          const highlighted =
-            selectedPoint && index < NUMBER_OF_SHOPS_TO_HIGHLIGHT;
-
-          return {
-            ...item,
-            highlighted,
-          };
-        });
-
-        setShops(cshops);
-      }
-    };
-
-    useEffect(() => {
-      if (typeof getClosestCoffeShops.current === "function" && selectedPoint) {
-        const orderedCoffeShops = getClosestCoffeShops.current(selectedPoint);
-
-        determineHighlightedShops(orderedCoffeShops);
-      }
-    }, [selectedPoint]);
-  };
-
-  useCoffeeShops();
+  const shops = useCoffeeShops(selectedPoint);
 
   return (
     <div className="app">
---
+++ closest-coffee-shops-ui/src/components/useCoffeeShops.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, useRef } from "react";
+import coffeeShops from "../services/closestCoffeeShops/coffeeShopsComponents/coffeeShops";
+import { NUMBER_OF_SHOPS_TO_HIGHLIGHT } from "../services/closestCoffeeShops/utils/config";
+
+export function useCoffeeShops(point) {
+  const [shops, setShops] = useState([]);
+  const getClosestCoffeShops = useRef(null);
+
+  useEffect(() => {
+    const displayInitialCoffeeShops = async () => {
+      const coffeeshops = await coffeeShops();
+
+      getClosestCoffeShops.current = coffeeshops.getCoffeeShops;
+
+      setShops(getSortedShopsRelativeTo({ x: 0, y: 0 }));
+    };
+
+    displayInitialCoffeeShops().catch(console.error);
+  }, []);
+
+  const getSortedShopsRelativeTo = (point) =>
+    getClosestCoffeShops.current(point);
+
+  const getHighlightedShops = (shops) => {
+    if (Array.isArray(shops)) {
+      const cshops = shops.map((item, index) => {
+        const highlighted = point && index < NUMBER_OF_SHOPS_TO_HIGHLIGHT;
+
+        return {
+          ...item,
+          highlighted,
+        };
+      });
+
+      return cshops;
+    }
+  };
+
+  const getHighlightedSortedShopsRelativeTo = (point) =>
+    getHighlightedShops(getSortedShopsRelativeTo(point));
+
+  useEffect(() => {
+    if (typeof getClosestCoffeShops.current === "function" && point) {
+      setShops(getHighlightedSortedShopsRelativeTo(point));
+    }
+  }, [point]);
+
+  return shops;
+}
