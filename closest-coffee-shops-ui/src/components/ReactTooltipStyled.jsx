43da98fd3386f887645167d7a085753d22a6a861
Razvan Filip <razvan.filip.70@gmail.com>
2022-08-25 15:23:10 +0300

    Update shop tooltip style

--- closest-coffee-shops-ui/src/components/ReactTooltipStyled.jsx
+++ closest-coffee-shops-ui/src/components/ReactTooltipStyled.jsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 
 export const ReactTooltipStyled = styled(ReactTooltip)`
   &.type-dark.place-top {
-    background-color: rgb(174, 20, 20);
+    background-image: linear-gradient(to top left, red, orange);
     padding: 0.3rem 1rem;
     font-size: 50px;
     border-radius: 50px;
     padding: 25px;
+    opacity: 0.9;
   }
 `;
