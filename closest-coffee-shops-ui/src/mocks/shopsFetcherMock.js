44fbc457844a2e338d106fce062b2eff745fe9dd
Razvan Filip <filip.razvan@agilefreaks.com>
2022-08-31 17:01:46 +0300

    Update shopsFetcherMock to also return status code

--- closest-coffee-shops-ui/src/mocks/shopsFetcherMock.js
+++ closest-coffee-shops-ui/src/mocks/shopsFetcherMock.js
@@ -1,16 +1,24 @@
 import { MOCKED_SHOPS_DATA } from "./mockedData";
 
 export const shopsFetcherMock = () => {
+  let jsdomAlert = null;
+
   const startMock = () => {
+    jsdomAlert = window.alert;
+    window.alert = () => {};
+
     jest.spyOn(global, "fetch").mockImplementation(() =>
       Promise.resolve({
-        json: () => Promise.resolve(MOCKED_SHOPS_DATA),
+        json: () => Promise.resolve([MOCKED_SHOPS_DATA, 200]),
       })
     );
   };
 
   const stopMock = () => {
     global.fetch.mockRestore();
+    window.alert = jsdomAlert;
+
+    jsdomAlert = null;
   };
 
   return { startMock, stopMock };
