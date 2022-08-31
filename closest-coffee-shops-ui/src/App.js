38e7351b0f70cae3fd1633f34080388ba9cf30fd
Razvan Filip <filip.razvan@agilefreaks.com>
2022-08-31 15:08:00 +0300

    Move provider from index.js to main component

--- closest-coffee-shops-ui/src/App.js
+++ closest-coffee-shops-ui/src/App.js
@@ -1,7 +1,13 @@
 import AppWrapper from "./components/AppWrapper";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
-  return <AppWrapper />;
+  return (
+    <Provider store={store}>
+      <AppWrapper />
+    </Provider>
+  );
 }
 
 export default App;
