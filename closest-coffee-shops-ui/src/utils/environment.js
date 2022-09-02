4c9f2100d0ccc40e4050a95490b24989ed43d4fb
Razvan Filip <filip.razvan@agilefreaks.com>
2022-09-02 16:02:25 +0300

    Remove redundant viewport to pixelsconverter function

--- closest-coffee-shops-ui/src/utils/environment.js
+++ closest-coffee-shops-ui/src/utils/environment.js
@@ -1,13 +1,3 @@
-import sizes from "../scss/map/_mapConstants.scss";
-
-const viewportToPixels = (value) => {
-  var parts = value.match(/([0-9\.]+)(vh|vw)/);
-  var q = Number(parts[1]);
-  var side =
-    window[["innerHeight", "innerWidth"][["vh", "vw"].indexOf(parts[2])]];
-  return Math.round(side * (q / 100));
-};
-
 export const getEnvironment = () => {
   const mapImage = document.getElementById("mapimage");
 
