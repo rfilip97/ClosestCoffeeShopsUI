aaadc2d41cb1ad3f6fe8bd72b08ed3d6240b31ef
Razvan Filip <filip.razvan@agilefreaks.com>
2022-10-24 15:38:03 +0300

    Change default coords of selectedPoint to map origin

--- closest-coffee-shops-ui/src/slices/selectedPointSlice.js
+++ closest-coffee-shops-ui/src/slices/selectedPointSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const selectedPointSlice = createSlice({
   name: "selectedPoint",
-  initialState: { selectedPoint: [] },
+  initialState: { selectedPoint: { x: "0", y: "0" } },
   reducers: {
     setSelectedPoint(state, action) {
       return { ...state, selectedPoint: action.payload };
