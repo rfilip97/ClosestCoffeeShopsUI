68fb1246252a15b858a552a79a44f681ebc04a98
Razvan Filip <filip.razvan@agilefreaks.com>
2022-10-24 11:33:02 +0300

    Add selectedPoint slice

--- closest-coffee-shops-ui/src/store.js
+++ closest-coffee-shops-ui/src/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineEpics, createEpicMiddleware } from "redux-observable";
 import shopReducer from "./slices/shopSlice";
+import selectedPointReducer from "./slices/selectedPointSlice";
 import updateShopsEpic from "./epics/updateShopsEpic";
 
 const epicMiddleware = createEpicMiddleware();
@@ -10,6 +11,7 @@ export const rootEpic = combineEpics(updateShopsEpic);
 export const store = configureStore({
   reducer: {
     shops: shopReducer,
+    selectedPoint: selectedPointReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(epicMiddleware),
