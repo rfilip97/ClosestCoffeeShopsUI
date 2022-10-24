83413a45fbcc45d486b86ed2b606ed694b219c18
Razvan Filip <filip.razvan@agilefreaks.com>
2022-10-24 14:52:25 +0300

    Return setShops action instead of dispatch in updateShopsEpic

--- closest-coffee-shops-ui/src/epics/updateShopsEpic.js
+++ closest-coffee-shops-ui/src/epics/updateShopsEpic.js
@@ -1,7 +1,5 @@
 import { ofType } from "redux-observable";
 import { map } from "rxjs/operators";
-import { setShops } from "../slices/shopSlice";
-import { store } from "../store";
 import { getHighlightedSortedShopsRelativeTo } from "../utils/shops";
 
 const updateShopsEpic = (action$, state) => {
@@ -13,10 +11,12 @@ const updateShopsEpic = (action$, state) => {
 
       if (shops) {
         const updatedShops = getHighlightedSortedShopsRelativeTo(point, shops);
-        store.dispatch(setShops(updatedShops));
-      }
 
-      action.type = "shops/setShopsComplete";
+        action.payload = updatedShops;
+        action.type = "shops/setShops";
+      } else {
+        action.type = "selectedPoint/setSelectedPointComplete";
+      }
 
       return action;
     })
