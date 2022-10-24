8ec06b82c26335c56c95882f4189c669fb20b271
Razvan Filip <filip.razvan@agilefreaks.com>
2022-10-24 11:33:22 +0300

    Dispatch selected point

--- closest-coffee-shops-ui/src/hooks/useCoffeeShops.jsx
+++ closest-coffee-shops-ui/src/hooks/useCoffeeShops.jsx
@@ -4,18 +4,26 @@ import { getHighlightedSortedShopsRelativeTo } from "../utils/shops";
 import { useDispatch } from "react-redux";
 
 import { setShops } from "../slices/shopSlice";
+import { setSelectedPoint } from "../slices/selectedPointSlice";
 
 export function useClosestCoffeeShops(point) {
   const shops = useSelector((state) => state.shops.shops);
+  const selectedPoint = useSelector((state) => state.selectedPoint.selectedPoint);
+
   const dispatch = useDispatch();
 
   const dispatchShops = (shops) => {
     dispatch(setShops(shops));
   };
 
+  const dispatchSelectedPoint = (point) => {
+    dispatch(setSelectedPoint(point));
+  };
+
   useEffect(() => {
     if (point) {
       dispatchShops(getHighlightedSortedShopsRelativeTo(point, shops));
+      dispatchSelectedPoint(point);
     }
   }, [point]);
 }
